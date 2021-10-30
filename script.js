600d85282f9a305aa9343e8deeee4d05c99ed90e
AYOOLA PAUL OLUWAMAYOWA <55549866+Ayopal@users.noreply.github.com>
2021-10-31 00:20:02 +0100

    files added

---
+++ script.js
@@ -0,0 +1,60 @@
+// BETTING SITE CHALLENGE
+
+'use strict';
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davis',
+      'Kimmich',
+      'Goretzka',
+      'Comman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', ' Gnarby', ' Lewandowski', ' Hummels'],
+  date: 'Nov 9th, 2023',
+  odds: {
+    team1: 7.33,
+    x: 3.35,
+    team2: 6.5,
+  },
+};
+
+//Things To Do
+
+//Creating Player Array for each team
+//  No 1
+
+// N0 2
+
+// No 3
+
+// N0 4 Create function for goals scored
+
+//No 5
+
+//No 6
+
+// No 7
